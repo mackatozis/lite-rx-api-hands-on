f6b51b971fc199c42f020d03b5434fc8d5b01843
Alexandros Efthymiadis <alexandros.efthymiadis@upstreamsystems.com>
2022-06-20 20:21:44 +0300

    Reformat code

--- src/test/java/io/pivotal/literx/Part02MonoTest.java
+++ src/test/java/io/pivotal/literx/Part02MonoTest.java
@@ -1,7 +1,6 @@
 package io.pivotal.literx;
 
 import java.time.Duration;
-
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
@@ -14,46 +13,46 @@ import reactor.test.StepVerifier;
  */
 public class Part02MonoTest {
 
-	Part02Mono workshop = new Part02Mono();
+    Part02Mono workshop = new Part02Mono();
 
 //========================================================================================
 
-	@Test
-	public void empty() {
-		Mono<String> mono = workshop.emptyMono();
-		StepVerifier.create(mono)
-				.verifyComplete();
-	}
+    @Test
+    public void empty() {
+        Mono<String> mono = workshop.emptyMono();
+        StepVerifier.create(mono)
+                .verifyComplete();
+    }
 
 //========================================================================================
 
-	@Test
-	public void noSignal() {
-		Mono<String> mono = workshop.monoWithNoSignal();
-		StepVerifier
-				.create(mono)
-				.expectSubscription()
-				.expectTimeout(Duration.ofSeconds(1))
-				.verify();
-	}
+    @Test
+    public void noSignal() {
+        Mono<String> mono = workshop.monoWithNoSignal();
+        StepVerifier
+                .create(mono)
+                .expectSubscription()
+                .expectTimeout(Duration.ofSeconds(1))
+                .verify();
+    }
 
 //========================================================================================
 
-	@Test
-	public void fromValue() {
-		Mono<String> mono = workshop.fooMono();
-		StepVerifier.create(mono)
-				.expectNext("foo")
-				.verifyComplete();
-	}
+    @Test
+    public void fromValue() {
+        Mono<String> mono = workshop.fooMono();
+        StepVerifier.create(mono)
+                .expectNext("foo")
+                .verifyComplete();
+    }
 
 //========================================================================================
 
-	@Test
-	public void error() {
-		Mono<String> mono = workshop.errorMono();
-		StepVerifier.create(mono)
-				.verifyError(IllegalStateException.class);
-	}
+    @Test
+    public void error() {
+        Mono<String> mono = workshop.errorMono();
+        StepVerifier.create(mono)
+                .verifyError(IllegalStateException.class);
+    }
 
 }
